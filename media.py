d3d69b3ebfcc8f9cf2e2ff2637139d3eaf0c1eaf
Chirag Ramani <chirag.ramani7@gmail.com>
2017-07-02 02:00:20 +0530

    Making code PEP8 compliant

--- media.py
+++ media.py
@@ -10,18 +10,17 @@ class Movie():
     """
     Class object stores movie related information
     """
-   
+
     def __init__(self, movie_title, movie_storyline,
                  movie_poster_image, movie_trailer_youtube):
         """
         initialize instance of class Movie
-        
         :param movie_title: title
         :param movie_storyline: storyline
         :param movie_poster_image: poster_image_url
         :param movie_trailer_youtube: trailer_youtube_url
         """
-        
+
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster_image
@@ -30,7 +29,6 @@ class Movie():
     def show_trailer(self):
         """
         initializing instance for opening the youtube video
-            
         :return: webbrowser to play thriller
         """
         webbrowser.open(self.trailer_youtube)
