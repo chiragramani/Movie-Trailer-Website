5c609b79dc8ce4cc513005a821134c9b40d373ac
Chirag Ramani <chirag.ramani7@gmail.com>
2017-07-02 01:36:55 +0530

    Using docstring for class, attributes and methods/instances instead of block comments. This is becuase a docstring can be accessed with the doc attribute in a different function or file.

--- media.py
+++ media.py
@@ -1,22 +1,36 @@
-# Importing module webbrowser since for show_trailer procedure, we need to open the url
+# coding = utf-8
+
+"""
+Module to display movie object, attributes and instances
+"""
 import webbrowser
 
-# Class - Movie
+
 class Movie():
-    # Constructor - initiliazing the instance of class Movie with necessary instance properties
-    def __init__(self, movie_title, movie_storyline, movie_poster_image, movie_trailer_youtube):
-        # The title of the movie
+    """
+    Class object stores movie related information
+    """
+   
+    def __init__(self, movie_title, movie_storyline,
+                 movie_poster_image, movie_trailer_youtube):
+        """
+        initialize instance of class Movie
+        
+        :param movie_title: title
+        :param movie_storyline: storyline
+        :param movie_poster_image: poster_image_url
+        :param movie_trailer_youtube: trailer_youtube_url
+        """
+        
         self.title = movie_title
-
-        # The storyline of the movie
         self.storyline = movie_storyline
-
-        # The link containing the poster_image
         self.poster_image_url = movie_poster_image
-
-        # The youtube link of the movie's trailer
         self.trailer_youtube_url = movie_trailer_youtube
 
-    # Opens the trailer in the default web-browser
     def show_trailer(self):
+        """
+        initializing instance for opening the youtube video
+            
+        :return: webbrowser to play thriller
+        """
         webbrowser.open(self.trailer_youtube)
